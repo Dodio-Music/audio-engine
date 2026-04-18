e3d81c872f4de832aacbbca99ffcde5b98d4c1b0
underswing <underswing187@gmail.com>
2026-04-18 19:36:56 +0200

    feat(main): add typescript support

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export function hello(): string;
+
+declare const _default: {
+    hello: () => string;
+}
+
+export default _default;
---
+++ index.mjs
@@ -0,0 +1,4 @@
+import native from "./index.cjs";
+
+export const hello = native.hello;
+export default native;
