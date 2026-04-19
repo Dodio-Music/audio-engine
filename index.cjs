f17073f673d3ec1f502b7ad81367c1c121ca8862
underswing <underswing187@gmail.com>
2026-04-19 22:36:28 +0200

    fix mjs export

--- index.cjs
+++ index.cjs
@@ -1,3 +1,5 @@
 const addon = require('./build/Release/dodio_audio.node');
 
+
 module.exports = addon;
+module.exports.AudioPlayer = addon.AudioPlayer;
--- index.mjs
+++ index.mjs
@@ -1,3 +1,4 @@
 import native from "./index.cjs";
 
+export const AudioPlayer = native.AudioPlayer;
 export default native;
